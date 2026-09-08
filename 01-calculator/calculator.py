482cccc03b9ac991e5706d879799f5d92b0a52ac
Dinun Ranasinghe <ranasinghedinun@gmail.com>
2026-09-08 11:21:09 +0530

    Calculator v1 bare minimum

--- 01-calculator/calculator.py
+++ 01-calculator/calculator.py
@@ -1,4 +1,30 @@
-# calculator
+# CLI calculator
 
-def add(x, y):
-    return x + y        
+print("Welcome to the CLI Calculator!")
+print("Use these arethmatic operators only (+, -, *, /)\n")
+
+user_input = input("Enter two numbers and the operator (e.g. 2 + 2): ")
+
+num1_str, operator, num2_str = user_input.split()
+
+num1 = float(num1_str)
+num2 = float(num2_str)
+
+if operator == "+":
+	result = num1 + num2
+elif operator == "-":
+	result = num1 - num2
+elif operator == "*":
+	result = num1 * num2
+elif operator == "/":
+	result = num1 / num2
+else:
+	print("Invalid operator")
+
+print(f"Your calculation : {result}")
+
+
+
+
+
+
