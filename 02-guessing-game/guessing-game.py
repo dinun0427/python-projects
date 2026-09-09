bd8bd1eda26567fa4e5a2dbc24e31cf9c1d55863
Dinun Ranasinghe <ranasinghedinun@gmail.com>
2026-09-09 13:20:58 +0530

    v1 completed with a loop for the random number guessing untill the user guess the correct one

--- 02-guessing-game/guessing-game.py
+++ 02-guessing-game/guessing-game.py
@@ -1,2 +1,22 @@
 # Number Guessing Game 
 
+import random
+
+computer_pick = random.randint(1, 100)
+
+while True:
+
+	user_input = int(input("Guess the number (Please enter an integer number between 1 and 100): "))
+
+	if user_input == computer_pick:
+		print("Hooray, You won. Congratulations")
+		break
+
+	elif user_input < computer_pick:
+		print("Too Low. Try again")
+		
+	elif user_input > computer_pick:
+		print("Too High. Try again")
+		
+	else:
+		raise ValueError
