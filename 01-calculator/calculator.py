d43b5a4e96aed56347912da05cee0b3a2fcfe13c
Dinun Ranasinghe <ranasinghedinun@gmail.com>
2026-09-09 10:58:58 +0530

    Add v4 final: exception-based error handling

--- 01-calculator/calculator.py
+++ 01-calculator/calculator.py
@@ -1,37 +1,66 @@
 # CLI calculator
 
-print("Welcome to the CLI Calculator!")
-print("Use these arithmetic operators only (+, -, *, /)\n")
 
-while True:
-    user_input = input("Enter two numbers and the operator (e.g. 2 + 2): ")
-    if user_input == "exit" or user_input == "quit":
-        print("Exiting the calculator. Goodbye!")
-        break
+def add(a, b):
+    result = a + b
+    return result
+
+
+def substract(a, b):
+    result = a - b
+    return result
+
+
+def multiply(a, b):
+    result = a * b
+    return result
+
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError
+    return a / b
+
+
+def calculate(a, b, operator):
+    if operator == "+":
+        return add(a, b)
+    elif operator == "-":
+        return substract(a, b)
+    elif operator == "*":
+        return multiply(a, b)
+    elif operator == "/":
+        return divide(a, b)
     else:
-        try:
-            num1_str, operator, num2_str = user_input.split()
+        raise ValueError(f"Invalid operator: {operator}")
 
-            num1 = float(num1_str)
-            num2 = float(num2_str)
-        except ValueError:
-            print("Error: Please enter valid numbers.")
-            continue
-        if operator == "+":
-            result = num1 + num2
-            print(f"Your calculation : {result}")
-        elif operator == "-":
-            result = num1 - num2
-            print(f"Your calculation : {result}")
-        elif operator == "*":
-            result = num1 * num2
-            print(f"Your calculation : {result}")
-        elif operator == "/":
+
+def main():
+    print("Welcome to the CLI Calculator!")
+    print("Use these arithmetic operators only (+, -, *, /)\n")
+
+    while True:
+        user_input = input("Enter two numbers and the operator (e.g. 2 + 2): ")
+        if user_input == "exit" or user_input == "quit":
+            print("Exiting the calculator. Goodbye!")
+            break
+        else:
             try:
-                result = num1 / num2
+                num1_str, operator, num2_str = user_input.split()
+
+                num1 = float(num1_str)
+                num2 = float(num2_str)
+            except ValueError:
+                print("Error: Please enter valid numbers.")
+                continue
+
+            try:
+                result = calculate(num1, num2, operator)
                 print(f"Your calculation : {result}")
             except ZeroDivisionError:
                 print("Error: Division by zero is not allowed.")
-                continue
-        else:
-            print("Invalid operator")
+            except ValueError as e:
+                print(f"Error: {e}")
+
+
+main()
