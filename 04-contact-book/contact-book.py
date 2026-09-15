8a36426bd805dbfd82c8394ec8ebbab9f1b3831b
Dinun Ranasinghe <ranasinghedinun@gmail.com>
2026-09-15 11:08:26 +0530

    Add v2: search contacts

--- 04-contact-book/contact-book.py
+++ 04-contact-book/contact-book.py
@@ -3,12 +3,12 @@ contact_book = []
 
 while True:
     try:
-        user_input_menu = int(input("Enter '1' to Add a Contact\n      '2' to View All Contacts\n      '3' to Quit Contact Book\n: "))
+        user_input_menu = int(input("Enter '1' to Add a Contact\n      '2' to View All Contacts\n      '3' to Quit Contact Book\n      '4' to Search a Contact\n: "))
         
-        if user_input_menu >3 or user_input_menu <1:
+        if user_input_menu >4 or user_input_menu <1:
             print("Error: Enter Valid Inputs.")
         
-        if user_input_menu == 1:
+        elif user_input_menu == 1:
             name = input("Enter Name: ")
             phone = (input("Enter Phone Number: "))
             email = input("Enter Email: ")
@@ -23,6 +23,15 @@ while True:
                 
         elif user_input_menu == 3:
             break
-                            
+        
+        elif user_input_menu == 4:
+            search = input("Search by name: ")
+            found = False
+            for contact in contact_book:
+                if search.lower() in contact['name'].lower():
+                    print(f"The user you are searching is : \n(Name: {contact['name']}\n Phone: {contact['phone']}\n Email: {contact['email']})")
+                    found = True
+            if not found:
+                print(f"No one named {search} found in contact book")                   
     except ValueError:
         print("Please enter a valid input")
